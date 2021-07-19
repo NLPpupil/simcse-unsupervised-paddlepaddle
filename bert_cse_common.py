d242cd2a3e2409b87fc8c37daf82c96e91475aac
nlp_pupil <nlp_pupil@163.com>
2021-07-19 16:16:16 +0800

    修改

--- bert_cse_common.py
+++ bert_cse_common.py
@@ -1,47 +1,20 @@
-import paddle
-import paddle.nn as nn
+from paddle import nn
+from paddle import to_tensor
+from paddlenlp.transformers import BertTokenizer
+from file_utils import read_config
 
-from file_utils import read_data
-from bert_cse_for_train import BertCseForTrain
-from paddle.io import DataLoader
-from dataset import SimCSEUnsupervisedDataset
-from paddle.static import InputSpec
-from bert_cse_common import config
-
-
-def train():
-    model = BertCseForTrain()
-    model.train()
-    epochs = int(config['num_epochs'])
-    batch_size = int(config['batch_size'])
-    optim = paddle.optimizer.Adam(learning_rate=0.00001, parameters=model.parameters())
-    train_dataset = SimCSEUnsupervisedDataset(read_data(config['data_path']))
-    cross_entropy = nn.loss.CrossEntropyLoss(use_softmax=True)
-    labels = paddle.arange(batch_size)
-
-    step = 0
-    verbose_every_n_steps = 10
-    for i in range(epochs):
-        train_dataset.shuffle()
-        print('epoch {0}/{1}'.format(i + 1, epochs))
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
-        for texts, dummy_label in train_loader():
-            sim = model(texts)
-            loss = cross_entropy(sim, labels)
-            loss.backward()
-            optim.step()
-            optim.clear_grad()
-            if step % verbose_every_n_steps == 0:
-                print('{0} steps loss={1}'.format(step, loss.numpy()[0]))
-            step += 1
-
-    path = "/root/paddlejob/workspace/output/simcse_model/bert_cse"
-    paddle.jit.save(
-        layer=model.bert,
-        path=path,
-        input_spec=[InputSpec(shape=[None, 128], dtype='int64')])
+config = read_config()
+cos_sim = nn.CosineSimilarity(axis=-1)
+tokenizer = BertTokenizer.from_pretrained(config['bert_name'])
+max_seq_len = int(config['max_seq_len'])
 
 
+def tokenize(texts):
+    tokenize_results = tokenizer(texts, max_seq_len=max_seq_len, pad_to_max_seq_len=True, return_token_type_ids=False)
+    input_ids = [r['input_ids'] for r in tokenize_results]
+    return to_tensor(input_ids)
 
-if __name__ == '__main__':
-    train()
+
+def avg_pooling(sequence_emb):
+    text_emb = sequence_emb.sum(1) / max_seq_len
+    return text_emb
